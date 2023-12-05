3839b680c82f1cf0be468fccd996a279358ea1e5
JoaoCardoso00 <jvcq13@gmail.com>
2023-12-05 16:47:23 -0300

    Initial Commit

---
+++ Todo-list/Todo_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_listApp.swift
+//  Todo-list
+//
+//  Created by joao cardoso on 05/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Todo_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
