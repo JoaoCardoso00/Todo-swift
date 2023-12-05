696ca457a2426fea77f10bc53589a98563920fa9
JoaoCardoso00 <jvcq13@gmail.com>
2023-12-05 18:49:38 -0300

    add checkbox component and change color names

---
+++ Todo-list/components/ToggleBox.swift
@@ -0,0 +1,18 @@
+//
+//  ToggleBox.swift
+//  Todo-list
+//
+//  Created by joao cardoso on 05/12/23.
+//
+
+import SwiftUI
+
+struct ToggleBox: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToggleBox()
+}
