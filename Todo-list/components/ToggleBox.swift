2e343dfd19f60a135d61037c7f49593d33ffd420
JoaoCardoso00 <jvcq13@gmail.com>
2023-12-05 18:49:57 -0300

    add checkbox component and change color names

--- Todo-list/components/ToggleBox.swift
+++ Todo-list/components/ToggleBox.swift
@@ -7,12 +7,39 @@
 
 import SwiftUI
 
+struct IOSCheckboxToggleStyle: ToggleStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        Button {
+            configuration.isOn.toggle()
+        } label: {
+            HStack {
+                Image(systemName: configuration.isOn ? "checkmark.circle" : "circle")
+                    .foregroundStyle(configuration.isOn ? Color("Gray-100") : Color("Brand Blue"))
+                    .background(configuration.isOn ? Color("Brand Purple dark") : Color(.clear))
+                    .clipShape(Circle())
+
+                configuration.label
+            }
+        }
+    }
+}
+
 struct ToggleBox: View {
+    @Binding var isChecked: Bool
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Toggle(isOn: $isChecked) {}.toggleStyle(IOSCheckboxToggleStyle())
+    }
+}
+
+struct ParentViewExample: View {
+    @State private var isChecked = false
+
+    var body: some View {
+        ToggleBox(isChecked: $isChecked)
     }
 }
 
 #Preview {
-    ToggleBox()
+    ParentViewExample()
 }
